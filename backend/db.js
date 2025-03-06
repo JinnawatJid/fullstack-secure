bff4ef0478fefc31e266cad306fae58f750afeeb
Jinnawat.Jid <jinnawat.jid@outlook.com>
2025-03-07 00:27:05 +0700

    add bcrypt

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,16 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-              
+  
 });
 
-pool.connect()
-    .then(() => {
-        console.log('Connected to PostgreSQL database');
-    })
-    .catch(err => {
-        console.error('Database connection error', err);
-    });
+pool
+  .connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL database");
+  })
+  .catch((err) => {
+    console.error("Database connection error", err);
+  });
 
-module.exports = pool; 
+module.exports = pool;
