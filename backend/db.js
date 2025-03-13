3b263d1fcf4daa82d698f3501d20e47651dd5369
Jinnawat.Jid <jinnawat.jid@outlook.com>
2025-03-13 18:16:41 +0700

    init member

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  
+});
+
+pool
+  .connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL database");
+  })
+  .catch((err) => {
+    console.error("Database connection error", err);
+  });
+
+module.exports = pool;
